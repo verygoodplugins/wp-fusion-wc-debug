1a6ff8fd86f040b8fc7db820651b67cd637c9e76
Jack <info@verygoodplugins.com>
2021-02-01 11:41:51 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== WP Fusion - WooCommerce Debug ===
+Contributors: verygoodplugins
+Tags: wp fusion, woocommerce
+Requires at least: 4.0
+Tested up to: 5.6
+Stable tag: 1.1
+
+Enables detailed debug logging for WP Fusion and WooCommerce to troubleshoot checkout issues.
+
+Install and activate, there are no configurable options.
+
+== Changelog ==
+
+= 1.0 - 2/1/2021 =
+* Initial release
---
+++ wp-fusion-wc-debug.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+Plugin Name: WP Fusion - WooCommerce Debug
+Description: Enables detailed debug logging for WP Fusion and WooCommerce to troubleshoot checkout issues.
+Plugin URI: https://wpfusion.com/
+Version: 1.0
+Author: Very Good Plugins
+Author URI: https://verygoodplugins.com/
+Text Domain: wp-fusion
+*/
+
+/**
+ * @copyright Copyright (c) 2016. All rights reserved.
+ *
+ * @license   Released under the GPL license http://www.opensource.org/licenses/gpl-license.php
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ *
+ */
+
+// deny direct access
+if ( ! function_exists( 'add_action' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+// IPN request
+
+function wpf_wc_debug_ipn_request( $posted ) {
+	wpf_log( 'info', 0, 'DEBUG: IPN request:', array( 'meta_array_nofilter' => $posted ) );
+}
+
+add_action( 'valid-paypal-standard-ipn-request', 'wpf_wc_debug_ipn_request' );
+
+// Pre payment complete
+
+function wpf_wc_debug_pre_payment_complete( $order_id ) {
+	wpf_log( 'info', 0, 'DEBUG: Pre payment complete: ' . $order_id );
+}
+
+add_action( 'woocommerce_pre_payment_complete', 'wpf_wc_debug_pre_payment_complete' );
+
+// Payment complete
+
+function wpf_wc_debug_payment_complete( $order_id ) {
+	wpf_log( 'info', 0, 'DEBUG: Payment complete: ' . $order_id );
+}
+
+add_action( 'woocommerce_payment_complete', 'wpf_wc_debug_payment_complete' );
+
+// Processing, priority 1
+
+function wpf_wc_debug_processing_1( $order_id ) {
+
+	if ( wp_doing_ajax() ) {
+		wpf_log( 'info', 0, 'DEBUG: Order status processing priority 1 / doing ajax: ' . $order_id );
+	} else {
+		wpf_log( 'info', 0, 'DEBUG: Order status processing priority 1 / NOT doing ajax: ' . $order_id );
+	}
+
+	if ( get_transient( 'wpf_woo_started_' . $order_id ) ) {
+		wpf_log( 'notice', 0, 'DEBUG: IS STARTED ' . $order_id );
+	} else {
+		wpf_log( 'info', 0, 'DEBUG: Is not started ' . $order_id );
+	}
+
+	global $wp_filter;
+
+	wpf_log( 'info', 0, 'DEBUG: Hooked actions for woocommerce_order_status_processing / ' . $order_id, array( 'meta_array_nofilter' => $wp_filter['woocommerce_order_status_processing'] ) );
+
+}
+
+add_action( 'woocommerce_order_status_processing', 'wpf_wc_debug_processing_1', 1 );
+
+// Processing, priority 5
+
+function wpf_wc_debug_processing_5( $order_id ) {
+	wpf_log( 'info', 0, 'DEBUG: Order status processing priority 5: ' . $order_id );
+}
+
+add_action( 'woocommerce_order_status_processing', 'wpf_wc_debug_processing_5', 5 );
+
+// Processing, priority 10
+
+function wpf_wc_debug_processing_10( $order_id ) {
+	wpf_log( 'info', 0, 'DEBUG: Order status processing priority 10: ' . $order_id );
+}
+
+add_action( 'woocommerce_order_status_processing', 'wpf_wc_debug_processing_10', 10 );
+
+// Processing, priority 15
+
+function wpf_wc_debug_processing_15( $order_id ) {
+	wpf_log( 'info', 0, 'DEBUG: Order status processing priority 15: ' . $order_id );
+}
+
+add_action( 'woocommerce_order_status_processing', 'wpf_wc_debug_processing_15', 15 );
+
